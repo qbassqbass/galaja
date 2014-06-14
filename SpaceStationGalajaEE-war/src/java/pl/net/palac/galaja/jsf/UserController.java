0420006f982b83a7649dcc684bfc071e66e516af
qbassqbass <jakub.palac@gmail.com>
2014-06-14 19:25:31 +0200

    o.o it works?

--- SpaceStationGalajaEE-war/src/java/pl/net/palac/galaja/jsf/UserController.java
+++ SpaceStationGalajaEE-war/src/java/pl/net/palac/galaja/jsf/UserController.java
@@ -8,8 +8,8 @@ import pl.net.palac.galaja.ejb.UserFacade;
 import java.io.Serializable;
 import java.util.ResourceBundle;
 import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
-import javax.enterprise.context.SessionScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
@@ -19,7 +19,7 @@ import javax.faces.model.ListDataModel;
 import javax.faces.model.SelectItem;
 
 @Named("userController")
-@SessionScoped
+@RequestScoped
 public class UserController implements Serializable {
 
     private User current;
